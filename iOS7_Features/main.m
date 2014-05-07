a797ea2575df36a124cf592c9769ee4adc512584
Aniket K <aniket.kulkarni56@gmail.com>
2014-05-07 11:55:08 +0530

    Initial Setup !

---
+++ iOS7_Features/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS7_Features
+//
+//  Created by Aniket K on 5/7/14.
+//  Copyright (c) 2014 AniketHub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ iOS7_FeaturesTests/iOS7_FeaturesTests.m
@@ -0,0 +1,34 @@
+//
+//  iOS7_FeaturesTests.m
+//  iOS7_FeaturesTests
+//
+//  Created by Aniket K on 5/7/14.
+//  Copyright (c) 2014 AniketHub. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS7_FeaturesTests : XCTestCase
+
+@end
+
+@implementation iOS7_FeaturesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
